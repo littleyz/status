69a24049b6ae1eb7a893c00b49ad111022990be4
YU Jiang <33709686+littleyz@users.noreply.github.com>
2022-05-24 14:51:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '服务状态',
+  SiteName: 'Server Status',
 
   // 站点链接
   SiteUrl: '/',
@@ -30,7 +30,7 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '首页',
+      text: 'Home',
       url: 'https://www.jiangyu.org'
     },
     {
